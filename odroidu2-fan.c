13eccd200054097a1ec12c6eecdb03bb187b4c65
Mauro Ribeiro <git@mdrjr.net>
2013-05-09 15:16:40 -0300

    More skeletons and file opening function

--- odroidu2-fan.c
+++ odroidu2-fan.c
@@ -6,3 +6,48 @@
  */
 
 
+#include <stdio.h>
+
+#include "odroidu2-fan.h"
+
+int openfiles(char *argv[]) {
+	fp_tmu = fopen(TMU_PATH, "r");
+	if(!fp_tmu) {
+		printf("%s: cannot open tmu file: %s\n", argv[0], TMU_PATH);
+		return 10;
+	}
+	fp_fan = fopen(FAN_PATH, "r");
+	if(!fp_fan) {
+		printf("%s: cannot open fan file: %s\n", argv[0], FAN_PATH);
+		return 11;
+	}
+	return 0;
+}
+
+char * gettemp() {
+	char *ret;
+
+	return ret;
+}
+
+int setfan(int temperature) {
+
+	return 0;
+}
+
+int main(int argc, char *argv[]) {
+
+	if(openfiles(argv) != 0) {
+		return -1;
+		exit(-1);
+	}
+
+/*	if(fork() == 0) {
+		while(1) {
+			sleep(1);
+			printf("teste\n");
+		}
+	}*/
+
+	return 0;
+}
---
+++ odroidu2-fan.h
@@ -0,0 +1,16 @@
+/*
+ * odroidu2-fan.c
+ *
+ *  Created on: May 9, 2013
+ *      Author: mdrjr
+ */
+
+#define	TMU_PATH "tmu"
+#define FAN_PATH "fan"
+
+/*
+ * Create both file handlers, one for the TMU and one for the FAN
+ */
+FILE *fp_tmu;
+FILE *fp_fan;
+
