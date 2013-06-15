e7297aaf315d49760bafc33080b4a55c55b1a43a
Mauro Ribeiro <git@mdrjr.net>
2013-06-15 15:52:23 -0300

    modified the TMU path for Kernel 3.8

--- odroidu2-fan.h
+++ odroidu2-fan.h
@@ -5,7 +5,7 @@
  *      Author: mdrjr
  */
 
-#define	TMU_PATH "/sys/devices/platform/tmu/temperature"
+#define	TMU_PATH "/sys/devices/virtual/thermal/thermal_zone0/temp"
 #define FAN_PATH "/sys/devices/platform/odroid-fan/pwm_duty"
 
 #define PROBES_PER_SECOND 1
